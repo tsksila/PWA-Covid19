67766fa3e31dbc5e2b51a774cbfb933b750a5633
ศิลา แก่งหลวง <sila29722@gmail.com>
2021-03-17 01:11:19 +0700

    fixbug

--- app.js
+++ app.js
@@ -81,7 +81,11 @@ app.get("/sw.js", function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
 
 /* app.get("/post/:title", function (req, res) {
